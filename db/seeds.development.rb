6c26bcaf77a5b680f243a09ec80302f3a4ed20d5
Nick Hammond <nick@nickhammond.com>
2023-07-17 12:43:41 -0600

    Update seeds to utilize application key

--- db/seeds.development.rb
+++ db/seeds.development.rb
@@ -6,21 +6,24 @@ app = Application.find_or_create_by(name: :apps)
 
 app.update(
   name: "Shipyrd",
+  key: "shipyrd",
   url: "https://shipyrd.io",
   repository_url: "https://github.com/nickhammond/shipyrd"
 )
 
 stages = %w[pre-connect pre-build pre-deploy post-deploy]
 
-stages.each do |stage|
-  Deploy.create(
-    recorded_at: Time.now,
-    status: "pre-deploy",
-    performer: "nick",
-    version: "7b3c0f04106366acfc7e1fcfe4b2e27f9667f8dc",
-    service_version: "apps@7b3c0f04",
-    command: "deploy",
-    destination: "staging",
-    runtime: (stage == "post-deploy") ? rand(60..119) : nil
-  )
+%w[staging production].each do |destination|
+  stages.each do |stage|
+    Deploy.create(
+      recorded_at: Time.now,
+      status: "pre-deploy",
+      performer: "nick",
+      version: "7b3c0f04106366acfc7e1fcfe4b2e27f9667f8dc",
+      service_version: "shipyrd@7b3c0f04",
+      command: "deploy",
+      destination: destination,
+      runtime: (stage == "post-deploy") ? rand(60..119) : nil
+    )
+  end
 end
