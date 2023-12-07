29ca1194ef59697ef460f413192794d0bf04b955
Nick Hammond <nick@nickhammond.com>
2023-12-07 07:59:22 -1000

    Add file extensions to logo and favicon

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.active_record.sqlite3_production_warning = false
 
   # Code is not reloaded between requests.
   config.enable_reloading = false
