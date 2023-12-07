c2a2b0285f9cc664ca974d4fa31b8d261f8ad81b
Nick Hammond <nick@nickhammond.com>
2023-12-07 07:30:20 -1000

    Test out anycable instead of postgres for streaming changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,10 +64,10 @@ Rails.application.configure do
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
 
   # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+  config.cache_store = :litecache
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
-  # config.active_job.queue_adapter     = :resque
+  config.active_job.queue_adapter = :litejob
   # config.active_job.queue_name_prefix = "shipyrd_production"
 
   # config.action_mailer.perform_caching = false
