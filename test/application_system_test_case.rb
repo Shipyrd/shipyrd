5ae1c40577332af06d0029da1f34fd712ff16ff2
Nick Hammond <nick@nickhammond.com>
2024-05-09 17:47:31 -0700

    Bump the capybara wait time for GH runners

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -4,6 +4,7 @@ require 'capybara/minitest'
 WebMock.allow_net_connect!
 
 Capybara.disable_animation = true
+Capybara.default_max_wait_time = 5
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
