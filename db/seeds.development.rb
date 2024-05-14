924360991d7b80056b98450003ad6066601f6d15
Nick Hammond <nick@nickhammond.com>
2024-05-14 05:07:04 -0700

    Render default destination if name missing

--- db/seeds.development.rb
+++ db/seeds.development.rb
@@ -2,6 +2,20 @@ ApiKey.find_or_create_by!(
   token: ENV["SHIPYRD_API_KEY"]
 )
 
+def create_deploy(application, destination, stage)
+  Deploy.create!(
+    recorded_at: Time.zone.now,
+    status: stage,
+    performer: "nickhammond",
+    commit_message: Faker::Hipster.sentence,
+    version: "7b3c0f04106366acfc7e1fcfe4b2e27f9667f8dc",
+    service_version: "#{application.key}@7b3c0f04",
+    command: "deploy",
+    destination: destination,
+    runtime: (stage == "post-deploy") ? rand(60..119) : nil
+  )
+end
+
 bacon = Application.find_or_create_by!(key: :bacon)
 
 bacon.update!(
@@ -30,24 +44,16 @@ applications = [bacon, eggs, ham]
 
 stages = %w[pre-connect pre-build pre-deploy post-deploy]
 
-applications.each do |application|
+applications[0..1].each do |application|
   %w[staging production].each do |destination|
     state = rand(1..3)
 
     (1..state).each do |i|
       stage = stages[i]
 
-      Deploy.create!(
-        recorded_at: Time.zone.now,
-        status: stage,
-        performer: "nickhammond",
-        commit_message: Faker::Hipster.sentence,
-        version: "7b3c0f04106366acfc7e1fcfe4b2e27f9667f8dc",
-        service_version: "#{application.key}@7b3c0f04",
-        command: "deploy",
-        destination: destination,
-        runtime: (stage == "post-deploy") ? rand(60..119) : nil
-      )
+      create_deploy(application, destination, stage)
     end
   end
 end
+
+create_deploy(applications[2], nil, "pre-deploy")
