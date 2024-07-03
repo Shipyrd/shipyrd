266c9f20b3b673781450601426a29130ac5ec456
Nick Hammond <nick@nickhammond.com>
2024-07-03 13:56:47 -0600

    Require additional secrets for AR encrypted fields support

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,9 @@
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
+ENV["SECRET_KEY_BASE"] = ENV["SHIPYRD_SECRET_KEY_BASE"]
+ENV["ENCRYPTION_DETERMINISTIC_KEY"] = ENV["SHIPYRD_ENCRYPTION_DETERMINISTIC_KEY"]
+ENV["ENCRYPTION_PRIMARY_KEY"] = ENV["SHIPYRD_ENCRYPTION_PRIMARY_KEY"]
+ENV["ENCRYPTION_KEY_DERIVATION_SALT"] = ENV["SHIPYRD_ENCRYPTION_KEY_DERIVATION_SALT"]
+
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
