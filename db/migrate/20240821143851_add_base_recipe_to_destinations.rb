164a4094eb4dbf031f0ae577a36969eece64cc4a
Nick Hammond <nick@nickhammond.com>
2024-08-22 14:07:10 -0700

    Add back in recipe on destinations migration

--- db/migrate/20240821143851_add_base_recipe_to_destinations.rb
+++ db/migrate/20240821143851_add_base_recipe_to_destinations.rb
@@ -1,5 +1,6 @@
 class AddBaseRecipeToDestinations < ActiveRecord::Migration[7.1]
   def change
     add_column :destinations, :base_recipe, :text
+    add_column :destinations, :recipe, :text
   end
 end
