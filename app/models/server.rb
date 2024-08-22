aafa36c98b0de583933c7a4b468286fcc8cbb86d
Nick Hammond <nick@nickhammond.com>
2024-08-22 13:49:29 -0700

    Remove the pending server run methods

--- app/models/server.rb
+++ app/models/server.rb
@@ -2,15 +2,4 @@ class Server < ApplicationRecord
   belongs_to :destination
 
   broadcasts
-
-  # def cli_options
-  #   name.present? ? "--destination #{name}" : nil
-  # end
-
-  # TODO
-  # def run_cli_command
-  #   destination.with_recipe do |base_recipe_path|
-  #     output = `kamal lock status -d production -c #{base_recipe_path}`
-  #   end
-  # end
 end
