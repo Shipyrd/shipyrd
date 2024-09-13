dcce0535955aa638c998e9d19931b14db99ed006
Nick Hammond <nick@nickhammond.com>
2024-09-12 17:25:45 -0700

    Bump schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_10_221450) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_10_221450) do
   create_table "api_keys", force: :cascade do |t|
     t.string "token"
     t.datetime "created_at", null: false
