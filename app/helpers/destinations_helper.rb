9052341984bd785ffdc9c8fc713ac04c1b837ef0
Nick Hammond <nick@nickhammond.com>
2024-09-14 08:20:32 -0700

    Always underline links, style destination edit and show pages with Bulma

--- app/helpers/destinations_helper.rb
+++ app/helpers/destinations_helper.rb
@@ -1,2 +1,12 @@
 module DestinationsHelper
+  def destination_tag(destination)
+    # Adjust v-alignment on destination show/edit
+    style = (params[:controller] == "destinations") ? "position: relative; top: -0.5rem;" : nil
+    label_type = destination.production? ? "is-danger" : "is-info"
+    klass = "tag has-text-weight-medium has-text-black my-auto is-medium is-light #{label_type}"
+
+    content_tag(:span, style: style, class: klass) do
+      link_to_if params[:action] != "edit", destination.display_name || "default", application_destination_path(destination.application, destination)
+    end
+  end
 end
