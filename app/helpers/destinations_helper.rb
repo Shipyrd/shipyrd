99763757df5686fcc25f19b2869ab879ee43b49e
Nick Hammond <nick@nickhammond.com>
2024-09-15 19:04:48 -0700

    Force wrap on runner code output, populate servers_count

--- app/helpers/destinations_helper.rb
+++ app/helpers/destinations_helper.rb
@@ -1,7 +1,7 @@
 module DestinationsHelper
   def destination_tag(destination)
     # Adjust v-alignment on destination show/edit
-    style = (params[:controller] == "destinations") ? "position: relative; top: -0.5rem;" : nil
+    style = %w[runners destinations].include?(params[:controller]) ? "position: relative; top: -0.5rem;" : nil
     label_type = destination.production? ? "is-danger" : "is-info"
     klass = "tag has-text-weight-medium has-text-black my-auto is-medium is-light #{label_type}"
 
--- db/migrate/20240916013929_add_counter_to_destinations.rb
+++ db/migrate/20240916013929_add_counter_to_destinations.rb
@@ -1,5 +1,8 @@
 class AddCounterToDestinations < ActiveRecord::Migration[7.2]
   def change
     add_column :destinations, :servers_count, :integer, default: 0
+    Destination.find_each do |destination|
+      destination.update(servers_count: destination.servers.count)
+    end
   end
 end
