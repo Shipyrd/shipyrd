cfa79ba6e2e227383db2301772dc8c0f4611e3f2
Nick Hammond <nick@nickhammond.com>
2024-09-18 17:07:15 -0700

    Add if_not_exists for solid_cable migration

--- db/cable_migrate/20240709193953_index_channels.solid_cable.rb
+++ db/cable_migrate/20240709193953_index_channels.solid_cable.rb
@@ -3,6 +3,6 @@
 # This migration comes from solid_cable (originally 20240607184711)
 class IndexChannels < ActiveRecord::Migration[7.1]
   def change
-    add_index :solid_cable_messages, :channel, length: 500
+    add_index :solid_cable_messages, :channel, length: 500, if_not_exists: true
   end
 end
