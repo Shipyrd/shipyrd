35aff7c6bf5616f0391936705fa24416dc97b83f
Nick Hammond <nick@nickhammond.com>
2024-09-18 17:44:25 -0700

    Remove additional work

--- db/cable_migrate/20240918204939_create_compact_channel.rb
+++ db/cable_migrate/20240918204939_create_compact_channel.rb
@@ -7,8 +7,6 @@ class CreateCompactChannel < ActiveRecord::Migration[7.2]
     add_column :solid_cable_messages, :channel_hash, :integer, limit: 8, if_not_exists: true
     add_index :solid_cable_messages, :channel_hash, if_not_exists: true
     change_column :solid_cable_messages, :payload, :binary, limit: 536_870_912, null: false
-
-    SolidCable::Message.destroy_all
     # standard:enable Rails/ReversibleMigration
   end
 end
