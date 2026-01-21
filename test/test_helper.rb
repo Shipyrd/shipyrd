9f3340aa0b6f08632625fb4eb3d3031fd65bb555
Nick Hammond <nick@nickhammond.com>
2026-01-21 15:31:39 -0700

    Add test hosts

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,7 @@
 ENV["RAILS_ENV"] ||= "test"
+ENV["SHIPYRD_HOST"] = "shipyrd.test"
+ENV["SHIPYRD_HOOKS_HOST"] = "hooks.shipyrd.test"
+
 require_relative "../config/environment"
 require "rails/test_help"
 require "webmock/minitest"
