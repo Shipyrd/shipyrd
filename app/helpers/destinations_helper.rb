c54c5a3237782ee422c71a175ecd7e452a24f254
Nick Hammond <nick@nickhammond.com>
2026-02-03 09:25:12 -0700

    Add badge display and copy to clipboard for both formats

--- app/helpers/destinations_helper.rb
+++ app/helpers/destinations_helper.rb
@@ -9,4 +9,51 @@ module DestinationsHelper
       link_to_if params[:action] != "edit", destination.display_name || "default", application_destination_path(destination.application, destination)
     end
   end
+
+  def destination_deployed_badge(destination, format = :markdown)
+    parameters = {
+      url: destination_badge_url(destination),
+      query: "deploy.time_ago",
+      label: "🚀 #{destination.name}",
+      color: destination.name == "production" ? "red" : nil
+    }
+
+    badge_url = "https://img.shields.io/badge/dynamic/json?" + parameters.to_query
+
+    if format == :markdown
+      "![#{destination.name}](#{badge_url})"
+    else
+      badge_url
+    end
+  end
+
+  def destination_locked_badge(destination, format = :markdown)
+    parameters = {
+      url: destination_badge_url(destination),
+      query: "lock.by",
+      color: destination.locked? ? "yellow" : "green",
+      # TODO: This can't be dynamic
+      label: "#{destination.locked? ? "🔒" : "🔓"} #{destination.name}"
+    }
+
+    badge_url = "https://img.shields.io/badge/dynamic/json?" + parameters.to_query
+
+    if format == :markdown
+      "![#{destination.name}](#{badge_url})"
+    else
+      badge_url
+    end
+  end
+
+  private
+
+  def destination_badge_url(destination)
+    badge_application_destination_url(
+      destination.application.badge_key,
+      destination.name,
+      # TODO: Deploy badge.shipyrd.io as a host in Cloudflare and kamal
+      host: "app.shipyrd.io",
+      format: :json
+    )
+  end
 end
--- app/views/badge/show.json.jbuilder
+++ app/views/badge/show.json.jbuilder
@@ -3,12 +3,9 @@ json.extract! @destination, :name
 if deploy = @destination.latest_post_deploy
   json.deploy do
     json.time_ago "#{time_ago_in_words(deploy.recorded_at)} ago"
-    json.performer deploy.performer
   end
 end
 
 json.lock do
-  json.by @destination.locked_by
-
-  json.time_ago @destination.locked_at.present? ? "#{time_ago_in_words(@destination.locked_at)} ago" : nil
+  json.by @destination.locked_by.present? ? @destination.locked_by : "unlocked"
 end
